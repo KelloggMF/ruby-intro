e8122370cb1f1e7aa1e9b2f1002e08985ba388a8
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-04-20 00:29:05 +0000

    Added practice code up until the break

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,39 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 2
 
 # Perform simple math with numbers
+puts 304994 + 23949 
+puts 1 * 442 
+puts 3 / 2.0
+puts 7 % 2 # remainder
+puts ""
+puts 5 * 2 - 2
 
 # Strings
+puts "Hello, world"
 
 # Combine strings together
+puts "Hello" + "," + " world"
+
+puts "Tacos" * 3
 
 # Variables
+food = "Tacos"
+number = 3
+
+puts food
+puts number
+puts food * number * 2
 
 # Combine strings and variables
+greeting = "Heyyyyy"
+welcome_message = "#{greeting}, world"
 
 # String manipulation
+puts welcome_message.upcase
+puts welcome_message.reverse
+puts welcome_message.upcase.reverse
+puts welcome_message.swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,39 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+puts true
+puts false
 
 # Boolean Expressions
+puts 3 == 2
+puts 3 > 2
+puts 3 != 2 # not equal
 
 # If Conditional Logic
+if 3 > 2
+# do something
+puts "math works"
+end
 
 # If/Else Conditional Logic
+password = "tacos"
+user_entered_password = "puppies"
+if password == user_entered_password
+    puts "welcome, you're in!!!"
+else puts "nope."
+end
 
 # Elsif Conditional Logic
+home_team = 1
+away_team = 1
+if home_team > away_team
+puts "winner!!!"
+elsif home_team == away_team #elsif, not elseif
+puts "tied...meh"
+else puts "loserrr :(" # only one that doesn't need a condition
+end
 
 # Combining Expressions
+# can combine if statements with &&
+# if temp > 60 && condition =="" sunny"
+# || is OR
